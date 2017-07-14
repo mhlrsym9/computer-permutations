582a88aa753e4a0ba0222233f63486235f716974
mdennehy <mdennehy@transparent.com>
2017-07-14 15:03:52 -0400

    Continue fun with destructuring.

--- src/computer_permutations/core.clj
+++ src/computer_permutations/core.clj
@@ -5,20 +5,23 @@
 (def motherboards
   (list {:name "ASRock Z270M Extreme4" :size "mATX" :optane? true :owned? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 0}
         {:name "Asus Z170 WS" :size "ATX" :optane? false :owned? true :thunderbolt-on-board? false :only-thunderbolt-card "Asus Thunderbolt3" :additional-cost 0}
-        {:name "ASRock Z270 Supercarrier" :size "ATX" :optane? true :owned? false :thunderbolt-on-board? true :only-cpu "i7-7700K" :only-case "Phanteks Pro" :additional-cost 350}
-        {:name "ASRock Z270 ITX/AC" :size "mITX" :optane? true :owned? false :thunderbolt-on-board? true :additional-cost 140}
-        {:name "Gigabyte GA-Z170X-Gaming 7" :size "ATX" :optane? false :owned? false :thunderbolt-on-board? true :additional-cost 107}
-        {:name "Gigabyte GA-Z270-UD5" :size "ATX" :optane? true :owned? false :thunderbolt-on-board? true :additional-cost 200}
-        {:name "Some other 100 series ATX MB" :size "ATX" :optane? false :owned? false :thunderbolt-on-board? false :additional-cost 100}
-        {:name "Some other 100 series mATX MB" :size "mATX" :optane? false :owned? false :thunderbolt-on-board? false :additional-cost 100}
-        {:name "Some other 200 series ATX MB" :size "ATX" :optane? true :owned? false :thunderbolt-on-board? false :additional-cost 100}
-        {:name "Some other 200 series mATX MB" :size "mATX" :optane? true :owned? false :thunderbolt-on-board? false :additional-cost 100}))
+        {:name "ASRock Z270 Supercarrier" :size "ATX" :optane? true :owned? false :thunderbolt-on-board? true :only-cpu "i7-7700K" :only-case "Phanteks Pro" :additional-cost 349.99}
+        {:name "ASRock Z270 ITX/AC" :size "mITX" :optane? true :owned? false :thunderbolt-on-board? true :additional-cost 138.99}
+        {:name "Gigabyte GA-Z170X-Gaming 7" :size "ATX" :optane? false :owned? false :thunderbolt-on-board? true :additional-cost (- (* 119.99 1.0625) 20.)}
+        {:name "Gigabyte GA-Z270-UD5" :size "ATX" :optane? true :owned? false :thunderbolt-on-board? true :additional-cost (* 199.99 1.0625)}
+        {:name "ASRock H170 Pro4S" :size "ATX" :optane? false :owned? false :thunderbolt-on-board? false :additional-cost 69.99}
+        {:name "Gigabyte GA-H110M-M.2" :size "mATX" :optane? false :owned? false :thunderbolt-on-board? false :additional-cost 46.99}
+        {:name "Gigabyte GA-B250-HD3" :size "ATX" :optane? true :owned? false :thunderbolt-on-board? false :additional-cost 59.99}
+        {:name "ASRock H270M Pro4" :size "mATX" :optane? true :owned? false :thunderbolt-on-board? false :additional-cost 64.99}))
 
 (def cases
   (list {:name "Phanteks Pro" :size "ATX" :additional-cost 0}
         {:name "Cooler Master Silencio 352" :size "mATX" :additional-cost 0}
         {:name "Phanteks P400S" :size "ATX" :additional-cost 0}
-        {:name "Some other ATX case" :size "ATX" :additional-cost 90}))
+        {:name "be quiet PURE BASE 600 - Black" :size "ATX" :additional-cost 89.90}
+        ;        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost (- (* 72.99 1.0625) 10.)}
+        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost 59.99}
+        {:name "Phanteks Enthoo Pro Titanium Green" :size "ATX" :additional-cost 69.99}))
 
 (def cpus
   (list {:name "i7-7700K" :licensed-to "Asus Z170 WS" :additional-cost 0}
@@ -28,17 +31,20 @@
 (def no-thunderbolt-card {:name "No Thunderbolt card" :additional-cost 0})
 
 (def thunderbolt-cards
-  (list {:name "ASRock Thunderbolt3" :additional-cost 80}
+  (list {:name "ASRock Thunderbolt3" :additional-cost 79.99}
         {:name "Asus Thunderbolt3" :additional-cost 0}
         no-thunderbolt-card))
 
+(def no-optane-card {:name "no optane" :optane? false :additional-cost 0})
+(def no-optane-card-name (:name no-optane-card))
+
 (def optane-cards
   (list {:name "32 GB optane" :optane? true :additional-cost 0}
-        {:name "no optane" :optane? false :additional-cost 0}))
+        no-optane-card))
 
 (def optical-drives
   (list {:name "long blu-ray" :additional-cost 0}
-        {:name "short blu-ray" :additional-cost 43}))
+        {:name "short blu-ray" :additional-cost 42.99}))
 
 (defn- optane-check? [mb optane-card]
   (let [optane-mb? (:optane? mb)
@@ -76,17 +82,18 @@
 (defn- all-different-cases? [l]
   (all-different-names? (map #(:name (:case %)) l)))
 
-(defn- valid-game-pc? [[game _ _]]
-  (let [mb-name (:name (:mb game))
-        case-name (:name (:case game))
-        cpu-name (:name (:cpu game))]
+(defn- valid-game-pc? [[{:keys [mb case cpu]} _ _]]
+  (let [[mb-name case-name cpu-name] (map :name (list mb case cpu))]
     (and (= "i7-7700K" cpu-name)
          (= "Phanteks Pro" case-name)
          (or (= "ASRock Z270 Supercarrier" mb-name)
              (= "Asus Z170 WS" mb-name)))))
 
-(defn- valid-media-pc? [[_ _ {{:keys [name]} :case}]]
-  (= "Phanteks P400S" name))
+(defn- valid-media-pc? [[_ _ {:keys [case optane-card]}]]
+  (let [case-name (:name case)
+        optane-card-name (:name optane-card)]
+    (and (= "Phanteks P400S" case-name)
+         (= no-optane-card-name optane-card-name))))
 
 (defn- are-two-mbs-owned? [l]
   (let [owned-l (map (fn [{{:keys [owned?]} :mb}] owned?) l)]
@@ -172,7 +179,7 @@
                                      {:mb mb :case case :cpu % :thunderbolt-card thunderbolt-card :optane-card optane-card :optical-drive optical-drive}) cpus)
         permutations-of-three-computers (create-all-pc-permutations permutations-per-cpu)
         names-of-permutations (map (fn [{:keys [game capture media total-additional-cost]}]
-                                     (clojure.string/join " " (list (str "Cost: " total-additional-cost "\n")
+                                     (clojure.string/join " " (list (format "Cost: %.2f\n" total-additional-cost)
                                                                     (produce-description "Game" game)
                                                                     (produce-description "Capture" capture)
                                                                     (produce-description "Media" media))))
