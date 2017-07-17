a85482603f890ffafab8d2f971f2a813ef7f64c5
mdennehy <mdennehy@transparent.com>
2017-07-17 17:05:05 -0400

    More accurately calculate lost costs. More accurately caclulate Windows license costs. Make sure cpu & mb have the same socket!

--- src/computer_permutations/core.clj
+++ src/computer_permutations/core.clj
@@ -2,46 +2,56 @@
   (:require [clojure.math.combinatorics :as combo])
   (:gen-class))
 
+(def asrock-z270m-extreme4-mb {:name "ASRock Z270M Extreme4" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :lost-cost 138.99})
+(def asrock-z270m-extreme4-mb-name (:name asrock-z270m-extreme4-mb))
+
+(def asus-z170-ws-mb {:name "Asus Z170 WS" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? false :only-thunderbolt-card "Asus Thunderbolt3" :lost-cost 266.87})
+(def asus-z170-ws-mb-name (:name asus-z170-ws-mb))
+
+(def core-2-duo-mb {:name "Core 2 Duo MB" :size "ATX" :socket 775 :optane? false :thunderbolt-on-board? false :only-cpu "Core 2 Duo E8400" :lost-cost 0})
+
 (def motherboards
-  (list {:name "ASRock Z270M Extreme4" :size "mATX" :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 0}
-        {:name "Asus Z170 WS" :size "ATX" :optane? false :thunderbolt-on-board? false :only-thunderbolt-card "Asus Thunderbolt3" :additional-cost 0}
-        {:name "Core 2 Duo MB" :size "ATX" :optane? false :thunderbolt-on-board? false :only-cpu "Core 2 Duo" :additional-cost 0}
+  (list asrock-z270m-extreme4-mb
+        asus-z170-ws-mb
+        core-2-duo-mb
         ;        {:name "Asus B150M-C D3" :size "mATX" :optane? false :thunderbolt-on-board? false :additional-cost 0}
 
-        {:name "Asus Z270 WS" :size "ATX" :optane? true :thunderbolt-on-board? false :only-cpu "i7-7700K" :only-thunderbolt-card "Asus Thunderbolt3" :additional-cost 379.99}
-        {:name "ASRock Z270 Supercarrier" :size "ATX" :optane? true :thunderbolt-on-board? true :only-cpu "i7-7700K" :only-case "Phanteks Pro" :additional-cost 349.99}
-        {:name "ASRock Z270 ITX/AC" :size "mITX" :optane? true :thunderbolt-on-board? true :additional-cost 138.99}
-        {:name "Gigabyte GA-Z170X-Gaming 7" :size "ATX" :optane? false :thunderbolt-on-board? true :additional-cost (- (* 119.99 1.0625) 20.) :from "MicroCenter"}
-        ;        {:name "Gigabyte GA-Z270-UD5" :size "ATX" :optane? true :thunderbolt-on-board? true :additional-cost (* 199.99 1.0625)}
-        ;        {:name "ASRock H170 Pro4S" :size "ATX" :optane? false :thunderbolt-on-board? false :additional-cost 69.99}
-        ;        {:name "Gigabyte GA-H110M-M.2" :size "mATX" :optane? false :thunderbolt-on-board? false :additional-cost 46.99}
-        ;        {:name "Gigabyte GA-B250-HD3" :size "ATX" :optane? true :thunderbolt-on-board? false :additional-cost 59.99}
-        ;        {:name "ASRock H270M Pro4" :size "mATX" :optane? true :thunderbolt-on-board? false :additional-cost 64.99 :from "NewEgg"}
-        ;        {:name "ASRock H270M Pro4" :size "mATX" :optane? true :thunderbolt-on-board? false :additional-cost 63.74 :from "MicroCenter"}
+        {:name "Asus Z270 WS" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-cpu "i7-7700K" :only-thunderbolt-card "Asus Thunderbolt3" :additional-cost 379.99}
+        {:name "ASRock Z270 Supercarrier" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? true :only-cpu "i7-7700K" :only-case "Phanteks Pro" :additional-cost 349.99}
+        {:name "ASRock Z270 ITX/AC" :size "mITX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost 138.99}
+        {:name "Gigabyte GA-Z170X-Gaming 7" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? true :additional-cost (- (* 119.99 1.0625) 20.) :from "MicroCenter"}
+        ;        {:name "Gigabyte GA-Z270-UD5" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost (* 199.99 1.0625)}
+        ;        {:name "ASRock H170 Pro4S" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 69.99}
+        ;        {:name "Gigabyte GA-H110M-M.2" :size "mATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 46.99}
+        ;        {:name "Gigabyte GA-B250-HD3" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? false :additional-cost 59.99}
+        ;        {:name "ASRock H270M Pro4" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :additional-cost 64.99 :from "NewEgg"}
+        ;        {:name "ASRock H270M Pro4" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :additional-cost 63.74 :from "MicroCenter"}
         ))
 
 (def cases
-  (list {:name "Phanteks Pro" :size "ATX" :additional-cost 0}
-        {:name "Cooler Master Silencio 352" :size "mATX" :additional-cost 0}
-        {:name "Phanteks P400S" :size "ATX" :additional-cost 0}
+  (list {:name "Phanteks Pro" :size "ATX" :lost-cost 84.99}
+        {:name "Cooler Master Silencio 352" :size "mATX" :lost-cost 57.99}
+        {:name "Phanteks P400S" :size "ATX" :lost-cost (+ 79.99 15.80 -8.80 2.99)}
 
         {:name "be quiet PURE BASE 600 - Black" :size "ATX" :additional-cost 89.90}
         ;        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost (- (* 72.99 1.0625) 10.) :from "MicroCenter"}
         {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost 59.99 :from "NewEgg"}
         {:name "Phanteks Enthoo Pro Titanium Green" :size "ATX" :additional-cost 69.99}))
 
-(def low-power-cpu {:name "i5-6500t" :optane? false :additional-cost 0})
+(def low-power-cpu {:name "i5-6500t" :socket 1151 :optane? false :lost-cost (+ 107.50 6.65)})
 (def low-power-cpu-name (:name low-power-cpu))
 
 (def cpus
-  (list {:name "i7-7700K" :licensed-to "Asus Z170 WS" :optane? true :additional-cost 0}
-        {:name "i5-7500" :licensed-to "ASRock Z270M Extreme4" :optane? true :additional-cost 0}
+  (list {:name "i7-7700K" :socket 1151 :licensed-to "Asus Z170 WS" :optane? true :lost-cost (* 299.99 1.0625)}
+        {:name "i5-7500" :socket 1151 :licensed-to "ASRock Z270M Extreme4" :optane? true :lost-cost (* 179.99 1.0625)}
         low-power-cpu
-        {:name "Core 2 Duo" :licensed-to "Core 2 Duo MB" :optane? false :additional-cost 0}
+        {:name "Core 2 Duo E8400" :socket 775 :licensed-to "Core 2 Duo MB" :optane? false :lost-cost 0}
         ))
 
-(def asus-thunderbolt-card {:name "Asus Thunderbolt3" :additional-cost 0})
-(def no-thunderbolt-card {:name "No Thunderbolt card" :additional-cost 0})
+(def asus-thunderbolt-card {:name "Asus Thunderbolt3" :lost-cost 59.49})
+(def asus-thunderbolt-card-name (:name asus-thunderbolt-card))
+
+(def no-thunderbolt-card {:name "No Thunderbolt card" :additional-cost 0 :lost-cost 0})
 (def no-thunderbolt-card-name (:name no-thunderbolt-card))
 
 (def thunderbolt-cards
@@ -49,23 +59,23 @@
         asus-thunderbolt-card
         no-thunderbolt-card))
 
-(def optane-card-32GB {:name "32 GB optane" :optane? true :additional-cost 0})
+(def optane-card-32GB {:name "32 GB optane" :optane? true :lost-cost (* 69.99 1.0625)})
 (def optane-card-32GB-name (:name optane-card-32GB))
 
-(def no-optane-card {:name "no optane" :optane? false :additional-cost 0})
+(def no-optane-card {:name "no optane" :optane? false :additional-cost 0 :lost-cost 0})
 (def no-optane-card-name (:name no-optane-card))
 
 (def optane-cards
   (list optane-card-32GB
         no-optane-card))
 
-(def no-optical-drive {:name "no optical drive" :additional-cost 0})
+(def no-optical-drive {:name "no optical drive" :additional-cost 0 :lost-cost 0})
 (def no-optical-drive-name (:name no-optical-drive))
 
 (def short-blu-ray {:name "short blu-ray" :additional-cost 42.99})
 (def short-blu-ray-name (:name short-blu-ray))
 
-(def long-blu-ray {:name "long blu-ray" :additional-cost 0})
+(def long-blu-ray {:name "long blu-ray" :lost-cost 0})
 (def long-blu-ray-name (:name long-blu-ray))
 
 (def optical-drives
@@ -90,9 +100,11 @@
           :else (= mb-size "mITX"))))
 
 (defn- cpu-check? [mb cpu]
-  (if-let [only-cpu (:only-cpu mb)]
-    (= only-cpu (:name cpu))
-    true))
+  (if (= (:socket mb) (:socket cpu))
+    (if-let [only-cpu (:only-cpu mb)]
+      (= only-cpu (:name cpu))
+      true)
+    false))
 
 (defn- optical-drive-check? [mb case optical-drive]
   (let [[mb-name case-name optical-drive-name] (map :name (list mb case optical-drive))]
@@ -138,8 +150,8 @@
   (= "Phanteks P400S" name))
 
 (defn- count-of-number-of-owned-motherboard-used [l]
-  (let [additional-cost-mbs (map #(get-in % [:mb :additional-cost]) l)]
-    (count (filter #(= 0 %) additional-cost-mbs))))
+  (let [lost-cost-mbs (map #(get-in % [:mb :lost-cost]) l)]
+    (count (filter identity lost-cost-mbs))))
 
 (defn- are-two-mbs-owned? [l]
   (= 2 (count-of-number-of-owned-motherboard-used l)))
@@ -157,28 +169,47 @@
 (defn- use-the-one-optane-card? [l]
   (= 1 (count-of-number-of-optane-cards-used l)))
 
-(defn is-6500t-cpu-used? [l]
+(defn- is-6500t-cpu-used? [l]
   (seq (filter #(= low-power-cpu-name %) (map (fn [c] (get-in c [:cpu :name])) l))))
 
+(defn- is-asrock-z270m-mb-used? [l]
+  (seq (filter #(= asrock-z270m-extreme4-mb-name %) (map (fn [c] (get-in c [:mb :name])) l))))
+
+(defn- is-asus-z170-ws-mb-used? [l]
+  (seq (filter #(= asus-z170-ws-mb-name %) (map (fn [c] (get-in c [:mb :name])) l))))
+
 (defn- already-licensed? [c]
-  (if-let [licensed-to (:licensed-to (:cpu c))]
-    (= licensed-to (:name (:mb c)))
+  (if-let [licensed-to (get-in c [:cpu :licensed-to])]
+    (= licensed-to (get-in c [:mb :name]))
     false))
 
-(defn- calculate-the-additional-cost [c]
-  (+ (reduce-kv (fn [m _ {:keys [additional-cost]}] (+ m additional-cost)) 0 c)
-     ; add cost of a Windows license
-     (if (already-licensed? c)
-       0
-       100)))
+(defn- calculate-additional-cost-of-pc [c]
+  (let [additional-cost-components-of-c (into {} (filter #(:additional-cost (val %)) c))]
+    (+ (reduce-kv (fn [m _ {:keys [additional-cost]}] (+ m additional-cost)) 0 additional-cost-components-of-c)
+       (if (already-licensed? c)
+         0
+         100))))
+
+(defn- is-at-least-one-pc-unlicensed? [l]
+  (nil? (seq (filter true? (map #(already-licensed? %) l)))))
 
 (defn- calculate-additional-cost [l]
-  (let [; if not using the paid-for optane card, add in its cost
-        optane-cost (if (= 0 (count-of-number-of-optane-cards-used l)) 80 0)
-        cpu-cost (if (is-6500t-cpu-used? l) 0 115)]
-    ; I already own one license so subtract that cost.
-    (- (apply + (cons cpu-cost (cons optane-cost (map calculate-the-additional-cost l))))
-       100)))
+  (- (reduce + (map calculate-additional-cost-of-pc l)) (if is-at-least-one-pc-unlicensed? 100 0)))
+
+(defn- calculate-lost-cost-of-missing-component [bag-of-components component l]
+  (let [look-for-lost-costs-in-bag (filter :lost-cost bag-of-components)
+        names-of-the-component (map #(get-in % [component :name]) l)]
+    (reduce + (map (fn [component]
+                     (let [name (:name component)]
+                       (if (seq (filter #(= name %) names-of-the-component))
+                         0.
+                         (:lost-cost component))))
+                   look-for-lost-costs-in-bag))))
+
+(defn- calculate-lost-cost [l]
+  (reduce + (map #(calculate-lost-cost-of-missing-component %1 %2 l)
+                 [motherboards cpus cases optane-cards optical-drives thunderbolt-cards]
+                 [:mb :cpu :case :optane-card :optical-drive :thunderbolt-card])))
 
 (defn- no-thunderbolt-card-in-pc? [{{:keys [name]} :thunderbolt-card}]
   (= (:name no-thunderbolt-card) name))
@@ -198,7 +229,9 @@
        (use-the-asus-thunderbolt-card? l)))
 
 (defn- create-pcs-map [[game capture media :as l]]
-  {:game game :capture capture :media media :total-additional-cost (calculate-additional-cost l)})
+  {:game game :capture capture :media media
+   :total-additional-cost (calculate-additional-cost l)
+   :total-lost-cost (calculate-lost-cost l)})
 
 (defn- create-all-pc-permutations [all-pcs]
   (->>
@@ -216,7 +249,8 @@
     (map create-pcs-map)))
 
 (defn- total-cost-comparator [el1 el2]
-  (< (:total-additional-cost el1) (:total-additional-cost el2)))
+  (< (+ (:total-additional-cost el1) (:total-lost-cost el1))
+     (+ (:total-additional-cost el2) (:total-lost-cost el2))))
 
 (defn- produce-description [desc c]
   (str "\t"
@@ -246,8 +280,8 @@
                                                        (optical-drive-check? mb case optical-drive))]
                                         {:mb mb :case case :cpu % :thunderbolt-card thunderbolt-card :optane-card optane-card :optical-drive optical-drive}) cpus)
         permutations-of-three-computers (create-all-pc-permutations permutations-per-cpu)
-        names-of-permutations (map (fn [{:keys [game capture media total-additional-cost]}]
-                                     (clojure.string/join " " (list (format "Cost: %.2f\n" total-additional-cost)
+        names-of-permutations (map (fn [{:keys [game capture media total-additional-cost total-lost-cost]}]
+                                     (clojure.string/join " " (list (format "Cost: %.2f [Additional Cost: %.2f Lost Cost: %.2f]\n" (+ total-additional-cost total-lost-cost) total-additional-cost total-lost-cost)
                                                                     (produce-description "Game" game)
                                                                     (produce-description "Capture" capture)
                                                                     (produce-description "Media" media))))
