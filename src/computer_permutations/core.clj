54334a61ee64d1dbd844e9f12f4dccdb16d76c22
MarkDennehy <mhlrsym9@comcast.net>
2017-07-18 23:13:53 -0400

    GamePC is only valid if it has a long blu-ray drive. Flip mb, cpu, optane, then check both tb permutations to determine whether media pc is thunderbolt capable. Use short blu ray for both Z270 Extreme4 boards.

--- src/computer_permutations/core.clj
+++ src/computer_permutations/core.clj
@@ -6,6 +6,9 @@
 (def asrock-z270m-extreme4-mb {:name "ASRock Z270M Extreme4" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :lost-cost 138.99})
 (def asrock-z270m-extreme4-mb-name (:name asrock-z270m-extreme4-mb))
 
+(def asrock-z270m-extreme4-mb-2 {:name "ASRock Z270M Extreme4 2" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 158.99})
+(def asrock-z270m-extreme4-mb-2-name (:name asrock-z270m-extreme4-mb-2))
+
 (def asus-z170-ws-mb {:name "Asus Z170 WS" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? false :only-thunderbolt-card "Asus Thunderbolt3" :lost-cost 266.87})
 (def asus-z170-ws-mb-name (:name asus-z170-ws-mb))
 
@@ -21,17 +24,17 @@
 (def motherboards
   (list asrock-z270m-extreme4-mb
         asus-z170-ws-mb
-        ;        core-2-duo-mb
+        core-2-duo-mb
 
-        ;        asus-z270-ws-mb
-        ;        asrock-z270-supercarrier-mb
+        asus-z270-ws-mb
+        asrock-z270-supercarrier-mb
 
-        ;        {:name "ASRock Z270 ITX/AC" :size "mITX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost 138.99}
+        {:name "ASRock Z270 ITX/AC" :size "mITX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost 138.99}
         {:name "Gigabyte GA-Z170X-Gaming 7" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? true :additional-cost (- (* 119.99 1.0625) 20.) :from "MicroCenter"}
-        ;        {:name "ASRock B150M Pro4" :size "mATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 63.99}
-        {:name "ASRock Z270M Extreme4 2" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 158.99}
+        {:name "ASRock B150M Pro4" :size "mATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 63.99}
+        asrock-z270m-extreme4-mb-2
 
-        ;        {:name "Gigabyte GA-Z270-UD5" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost (* 199.99 1.0625)}
+        {:name "Gigabyte GA-Z270-UD5" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost (* 199.99 1.0625)}
         ;        {:name "ASRock H170 Pro4S" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 69.99}
         ;        {:name "Gigabyte GA-H110M-M.2" :size "mATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 46.99}
         ;        {:name "Gigabyte GA-B250-HD3" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? false :additional-cost 59.99}
@@ -49,7 +52,7 @@
 
         {:name "be quiet PURE BASE 600 - Black" :size "ATX" :additional-cost 89.90}
         ;        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost (- (* 72.99 1.0625) 10.) :from "MicroCenter"}
-        ;        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost 59.99 :from "NewEgg"}
+        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost 59.99 :from "NewEgg"}
         ;        {:name "Phanteks Enthoo Pro Titanium Green" :size "ATX" :additional-cost 69.99}
         ))
 
@@ -65,9 +68,7 @@
 (def old-cpu {:name "Core 2 Duo E8400" :socket 775 :licensed-to core-2-duo-mb-name :optane? false :lost-cost 0})
 (def old-cpu-name (:name old-cpu))
 
-(def cpus (list game-cpu high-power-i5-cpu low-power-cpu
-                ;                old-cpu
-                ))
+(def cpus (list game-cpu high-power-i5-cpu low-power-cpu old-cpu))
 
 (def asrock-thunderbolt3-card {:name "ASRock Thunderbolt3" :additional-cost 79.99})
 (def asrock-thunderbolt3-card-name (:name asrock-thunderbolt3-card))
@@ -125,7 +126,8 @@
 (defn- optical-drive-check? [mb case optical-drive]
   (let [[mb-name case-name optical-drive-name] (map :name (list mb case optical-drive))]
     (or (not= long-blu-ray-name optical-drive-name)
-        (not= asrock-z270m-extreme4-mb-name mb-name)
+        (and (not= asrock-z270m-extreme4-mb-name mb-name)
+             (not= asrock-z270m-extreme4-mb-2-name mb-name))
         (not= silencio-case-name case-name))))
 
 (defn- all-different-names? [names]
@@ -144,12 +146,12 @@
   (let [[mb-name case-name cpu-name optical-drive-name] (map :name (list mb case cpu optical-drive))]
     (and (= game-cpu-name cpu-name)
          (= "Phanteks Pro" case-name)
-         (not= no-optical-drive-name optical-drive-name)
+         (= long-blu-ray-name optical-drive-name)
          (or (= asrock-z270-supercarrier-mb-name mb-name)
              (= asus-z170-ws-mb-name mb-name)
              (= asus-z270-ws-mb-name mb-name)))))
 
-(defn- valid-capture-pc? [[ _ {:keys [optical-drive mb]} _]]
+(defn- valid-capture-pc? [[_ {:keys [optical-drive mb]} _]]
   (let [optical-drive-name (:name optical-drive)
         socket (:socket mb)]
     (and (= 1151 socket)
@@ -246,16 +248,21 @@
                             (= asus-thunderbolt-card-name name)) l)]
     (or (empty? asus-mbs) (= 1 (count asus-tbs)))))
 
+(defn- flip-components [pc1 pc2]
+  (apply assoc pc1 (flatten (map #(list % (% pc2)) (list :mb :cpu :optane-card)))))
+
+(defn- is-flipped-capture-valid? [flipped-capture]
+  (and (thunderbolt-card-check? (:mb flipped-capture) (:thunderbolt-card flipped-capture))
+       (size-check? (:mb flipped-capture) (:case flipped-capture))
+       (optical-drive-check? (:mb flipped-capture) (:case flipped-capture) (:optical-drive flipped-capture))
+       (valid-capture-pc? (list nil flipped-capture nil))
+       (is-thunderbolt-pc? flipped-capture)))
+
 (defn- does-flipped-capture-and-media-pcs-still-work? [[game capture media]]
   (if (= (get-in capture [:cpu :name]) high-power-i5-cpu-name)
-    (let [flipped-capture (assoc capture :mb (:mb media)
-                                         :cpu (:cpu media))
-          flipped-media (assoc media :mb (:mb capture)
-                                     :cpu (:cpu capture))]
-      (and (size-check? (:mb flipped-capture) (:case flipped-capture))
-           (optical-drive-check? (:mb flipped-capture) (:case flipped-capture) (:optical-drive flipped-capture))
-           (valid-capture-pc? (list game flipped-capture flipped-media))
-           (is-thunderbolt-pc? flipped-capture)))
+    (let [flipped-capture (flip-components capture media)
+          flipped-capture2 (assoc flipped-capture :thunderbolt-card (:thunderbolt-card media))]
+      (seq (filter true? (map is-flipped-capture-valid? (list flipped-capture flipped-capture2)))))
     true))
 
 (defn- is-media-pc-thunderbolt-capable-backup-to-capture-pc? [[_ capture media]]
