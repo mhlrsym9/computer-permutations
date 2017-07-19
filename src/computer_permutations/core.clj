b897e58697d3cffdf6811714339cc8664d3da545
MarkDennehy <mhlrsym9@comcast.net>
2017-07-19 02:09:17 -0400

    Significantly reduce search time by dividing up the initial pc collection into game (i7-700k) and all others. Time goes from > 1 hr to less than 1 minute. Add a couple of additional boards...
    Decided on ASRock H270 Pro4 as it can use TB if needed.
    Will need to test whether i5-7500 or i5-6500t cheaper for rip/capture tasks. Only experimentation will tell.

--- src/computer_permutations/core.clj
+++ src/computer_permutations/core.clj
@@ -6,7 +6,7 @@
 (def asrock-z270m-extreme4-mb {:name "ASRock Z270M Extreme4" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :lost-cost 138.99})
 (def asrock-z270m-extreme4-mb-name (:name asrock-z270m-extreme4-mb))
 
-(def asrock-z270m-extreme4-mb-2 {:name "ASRock Z270M Extreme4 2" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 158.99})
+(def asrock-z270m-extreme4-mb-2 {:name "ASRock Z270M Extreme4 2" :size "mATX" :socket 1151 :optane? true :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 197.95})
 (def asrock-z270m-extreme4-mb-2-name (:name asrock-z270m-extreme4-mb-2))
 
 (def asus-z170-ws-mb {:name "Asus Z170 WS" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? false :only-thunderbolt-card "Asus Thunderbolt3" :lost-cost 266.87})
@@ -31,7 +31,7 @@
 
         {:name "ASRock Z270 ITX/AC" :size "mITX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost 138.99}
         {:name "Gigabyte GA-Z170X-Gaming 7" :size "ATX" :socket 1151 :optane? false :thunderbolt-on-board? true :additional-cost (- (* 119.99 1.0625) 20.) :from "MicroCenter"}
-        {:name "ASRock B150M Pro4" :size "mATX" :socket 1151 :optane? false :thunderbolt-on-board? false :additional-cost 63.99}
+        {:name "ASRock H270 Pro4" :size "mTX" :socket 1151 :optane? false :thunderbolt-on-board? false :only-thunderbolt-card "ASRock Thunderbolt3" :additional-cost 89.99}
         asrock-z270m-extreme4-mb-2
 
         {:name "Gigabyte GA-Z270-UD5" :size "ATX" :socket 1151 :optane? true :thunderbolt-on-board? true :additional-cost (* 199.99 1.0625)}
@@ -52,7 +52,7 @@
 
         {:name "be quiet PURE BASE 600 - Black" :size "ATX" :additional-cost 89.90}
         ;        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost (- (* 72.99 1.0625) 10.) :from "MicroCenter"}
-        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost 59.99 :from "NewEgg"}
+        ;        {:name "Corsair Carbide Series 300R" :size "ATX" :additional-cost 59.99 :from "NewEgg"}
         ;        {:name "Phanteks Enthoo Pro Titanium Green" :size "ATX" :additional-cost 69.99}
         ))
 
@@ -259,11 +259,9 @@
        (is-thunderbolt-pc? flipped-capture)))
 
 (defn- does-flipped-capture-and-media-pcs-still-work? [[game capture media]]
-  (if (= (get-in capture [:cpu :name]) high-power-i5-cpu-name)
-    (let [flipped-capture (flip-components capture media)
-          flipped-capture2 (assoc flipped-capture :thunderbolt-card (:thunderbolt-card media))]
-      (seq (filter true? (map is-flipped-capture-valid? (list flipped-capture flipped-capture2)))))
-    true))
+  (let [flipped-capture (flip-components capture media)
+        flipped-capture2 (assoc flipped-capture :thunderbolt-card (:thunderbolt-card media))]
+    (some is-flipped-capture-valid? (list flipped-capture flipped-capture2))))
 
 (defn- is-media-pc-thunderbolt-capable-backup-to-capture-pc? [[_ capture media]]
   (if (= (get-in capture [:cpu :name]) high-power-i5-cpu-name)
@@ -278,7 +276,8 @@
        (is-thunderbolt-pc? capture)
        (no-thunderbolt-card-in-pc? media)
        (use-the-one-asus-thunderbolt-card-if-possible? l)
-       (does-flipped-capture-and-media-pcs-still-work? l)))
+       (does-flipped-capture-and-media-pcs-still-work? l)
+       ))
 
 (defn- create-pcs-map [[game capture media :as l]]
   (let [lost-data (calculate-lost-cost l)]
@@ -289,19 +288,19 @@
      :total-lost-cost       (calculate-lost-cost l)}))
 
 (defn- create-all-pc-permutations [all-pcs]
-  (->>
-    (mapcat #(combo/permutations %) (combo/combinations all-pcs 3))
-    ;    (mapcat #(apply combo/cartesian-product %) (combo/permutations l))
-    (filter all-different-mbs?)
-    (filter all-different-cases?)
-    (filter all-different-cpus?)
-    (filter valid-game-pc?)
-    (filter valid-capture-pc?)
-    (filter valid-media-pc?)
-    (filter are-at-least-two-mbs-owned?)
-    (filter is-correct-thunderbolt-configuration?)
-    (filter at-most-one-optane-card?)
-    (map create-pcs-map)))
+  (let [game-pcs (filter (fn [c] (valid-game-pc? (list c nil nil))) (filter #(= game-cpu-name (get-in % [:cpu :name])) all-pcs))
+        other-pcs (remove #(= game-cpu-name (get-in % [:cpu :name])) all-pcs)]
+    (->>
+      (map flatten (combo/cartesian-product game-pcs (mapcat #(combo/permutations %) (combo/combinations other-pcs 2))))
+      (filter all-different-mbs?)
+      (filter all-different-cases?)
+      (filter all-different-cpus?)
+      (filter valid-capture-pc?)
+      (filter valid-media-pc?)
+      (filter are-at-least-two-mbs-owned?)
+      (filter is-correct-thunderbolt-configuration?)
+      (filter at-most-one-optane-card?)
+      (map create-pcs-map))))
 
 (defn- total-cost-comparator [el1 el2]
   (< (+ (get-in el1 [:total-additional-cost :cost]) (get-in el1 [:total-lost-cost :cost]))
